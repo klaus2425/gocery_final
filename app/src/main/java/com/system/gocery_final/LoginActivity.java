95be02ece894446c356c0d8b6c808d10e51b2f93
Jaycie <jayciegdelacruz@gmail.com>
2023-04-18 20:17:18 +0800

    Update LoginActivity.java

--- app/src/main/java/com/system/gocery_final/LoginActivity.java
+++ app/src/main/java/com/system/gocery_final/LoginActivity.java
@@ -3,12 +3,45 @@ package com.system.gocery_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText inputEmail, inputPassword;
+    private Button loginButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton = (Button) findViewById(R.id.login_button);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser(){
+        String email  = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        if(TextUtils.isEmpty(password) || TextUtils.isEmpty(email) ) {
+            Toast.makeText(LoginActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+        }
+
+        allowAccessToAccount(email, password);
+    }
+
+    private void allowAccessToAccount(String email, String password){
+        
     }
 }
