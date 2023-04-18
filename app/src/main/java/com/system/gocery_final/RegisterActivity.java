73136be8d871307154eba0d46c340c8f2ac79ad6
Jaycie <jayciegdelacruz@gmail.com>
2023-04-18 18:00:58 +0800

    Update Login and Registration

--- app/src/main/java/com/system/gocery_final/RegisterActivity.java
+++ app/src/main/java/com/system/gocery_final/RegisterActivity.java
@@ -3,12 +3,34 @@ package com.system.gocery_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
+
+    private Button signUpButton;
+    private EditText inputFirstName, inputLastName, inputEmail, inputPassword, inputConfirmPassword, inputAddress, inputContactNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+        inputFirstName = (EditText) findViewById(R.id.firstName);
+        inputLastName = (EditText) findViewById(R.id.lastName);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        inputConfirmPassword = (EditText) findViewById(R.id.confirm_password);
+        inputAddress = (EditText) findViewById(R.id.address);
+        inputContactNumber = (EditText) findViewById(R.id.contactNumber);
+
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
