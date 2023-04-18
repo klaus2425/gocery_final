ec8614a31c3251470776033bf71e1fe7cd033850
Jaycie <jayciegdelacruz@gmail.com>
2023-04-18 17:40:21 +0800

    Update files

--- app/src/main/java/com/system/gocery_final/MainActivity.java
+++ app/src/main/java/com/system/gocery_final/MainActivity.java
@@ -2,13 +2,35 @@ package com.system.gocery_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button registerButton, loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerButton = (Button) findViewById(R.id.main_register_btn);
+        loginButton = (Button) findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            }
+        });
     }
 }
