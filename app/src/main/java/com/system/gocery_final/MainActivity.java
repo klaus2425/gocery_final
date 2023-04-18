c2565ff05790edd8e1791bffca2768241f57815e
Overseer24 <66957205+Overseer24@users.noreply.github.com>
2023-04-19 04:29:45 +0800

    Update MainActivity.java

--- app/src/main/java/com/system/gocery_final/MainActivity.java
+++ app/src/main/java/com/system/gocery_final/MainActivity.java
@@ -1,23 +1,45 @@
 package com.system.gocery_final;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.system.gocery_final.Model.Users;
+import com.system.gocery_final.Prevalent.Prevalent;
+
+import io.paperdb.Paper;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressDialog loadingBar;
+    private String parentDbName = "Users";
     private Button registerButton, loginButton;
-
+    private FirebaseUser user;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Paper.init(this);
+        loadingBar = new ProgressDialog(this);
         registerButton = (Button) findViewById(R.id.main_register_btn);
         loginButton = (Button) findViewById(R.id.main_login_btn);
-
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,5 +54,54 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
+
+        String UserEmailKey = Paper.book().read(Prevalent.userEmail);
+        String UserPasswordKey = Paper.book().read(Prevalent.userPasswordKey);
+        if(UserEmailKey != "" && UserPasswordKey != ""){
+            if(!TextUtils.isEmpty(UserEmailKey) && !TextUtils.isEmpty(UserPasswordKey)){
+                AllowAccess(UserEmailKey,UserPasswordKey);
+                loadingBar.setTitle("Already Logged in");
+                loadingBar.setMessage("Please wait .....");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+            }
+        }
+
+    }
+    private void AllowAccess(final String email, final String password) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.child(parentDbName).child(user.getUid()).exists()){
+                    Users userData = snapshot.child(parentDbName).child(user.getUid()).getValue(Users.class);
+                    if(userData.getEmail().equals(email)){
+                        if (userData.getPassword().equals(password)) {
+                            Toast.makeText(MainActivity.this, "Log in successfully...", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        } else {
+                            loadingBar.dismiss();
+                            Toast.makeText(MainActivity.this, "Password is incorrect...", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Account" + email +  "do not exist.", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
