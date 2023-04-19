befe45ccba75af96325a9109065f46a1229fee73
Jaycie <jayciegdelacruz@gmail.com>
2023-04-19 21:28:00 +0800

    Start image upload

--- app/src/main/java/com/system/gocery_final/AdminAddNewProductActivity.java
+++ app/src/main/java/com/system/gocery_final/AdminAddNewProductActivity.java
@@ -1,8 +1,11 @@
 package com.system.gocery_final;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,19 +18,32 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
     private Button addNewProductButton;
     private EditText inputProductName, inputProductDescription, inputProductPrice, inputProductQuantity;
     ImageView inputProductImage;
+    private static final int galleryPic = 1;
+    ActivityResultLauncher<String> mTakePhoto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
-
         categoryName = getIntent().getExtras().get("category").toString(); // Get category from AdminCategoryActivity
-
         addNewProductButton = (Button) findViewById(R.id.add_new_product);
         inputProductImage = (ImageView) findViewById(R.id.select_product_image);
         inputProductName = (EditText) findViewById(R.id.product_name);
         inputProductDescription = (EditText) findViewById(R.id.product_description);
         inputProductPrice = (EditText) findViewById(R.id.product_price);
         inputProductQuantity = (EditText) findViewById(R.id.product_quantity);
+        inputProductImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenGallery();
+            }
+        });
+
 
     }
+
+    private void OpenGallery() {
+
+
+    }
+
 }
