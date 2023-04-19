15164f0cfa0cdc88c6f174c56db719a531b82c5d
Jaycie <jayciegdelacruz@gmail.com>
2023-04-19 18:53:01 +0800

    Add add product fuinction design

--- app/src/main/java/com/system/gocery_final/AdminAddNewProductActivity.java
+++ app/src/main/java/com/system/gocery_final/AdminAddNewProductActivity.java
@@ -3,11 +3,18 @@ package com.system.gocery_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
-    private String categoryName; //CategoryName
+
+    private String categoryName;
+    private Button addNewProductButton;
+    private EditText inputProductName, inputProductDescription, inputProductPrice, inputProductQuantity;
+    ImageView inputProductImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +22,12 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
 
         categoryName = getIntent().getExtras().get("category").toString(); // Get category from AdminCategoryActivity
 
-        Toast.makeText(this,categoryName, Toast.LENGTH_SHORT).show();
+        addNewProductButton = (Button) findViewById(R.id.add_new_product);
+        inputProductImage = (ImageView) findViewById(R.id.select_product_image);
+        inputProductName = (EditText) findViewById(R.id.product_name);
+        inputProductDescription = (EditText) findViewById(R.id.product_description);
+        inputProductPrice = (EditText) findViewById(R.id.product_price);
+        inputProductQuantity = (EditText) findViewById(R.id.product_quantity);
 
     }
 }
