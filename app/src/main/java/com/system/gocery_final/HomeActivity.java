5fc60bdf2c937b9cff795f30983fee0e52476ae7
Jaycie <jayciegdelacruz@gmail.com>
2023-04-23 16:13:18 +0800

    Create settings design
    Natapos ko part 17

--- app/src/main/java/com/system/gocery_final/HomeActivity.java
+++ app/src/main/java/com/system/gocery_final/HomeActivity.java
@@ -176,8 +176,8 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         }
         else if (id == R.id.nav_settings)
         {
-//            Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
-//            startActivity(intent);
+            Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
+            startActivity(intent);
         }
         else if (id == R.id.nav_logout)
         {
