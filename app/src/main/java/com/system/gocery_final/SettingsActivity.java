61119b971caafdc5fbcec1b8e69302c12503119c
Overseer24 <66957205+Overseer24@users.noreply.github.com>
2023-04-23 21:28:12 +0800

    Update SettingsActivity.java

--- app/src/main/java/com/system/gocery_final/SettingsActivity.java
+++ app/src/main/java/com/system/gocery_final/SettingsActivity.java
@@ -165,7 +165,7 @@ public class SettingsActivity extends AppCompatActivity {
                         userMap.put("image",myUrl);
                         ref.child(Prevalent.currentOnlineUser.getEmail()).updateChildren(userMap);
 
-                        startActivity(new Intent(SettingsActivity.this,MainActivity.class));
+                        startActivity(new Intent(SettingsActivity.this,HomeActivity.class));
                         Toast.makeText(SettingsActivity.this,"Profile Info Update Successfully",Toast.LENGTH_SHORT);
                         finish();
                     }
