40c25ed42e21991e48909c72de679b8ff273b3db
Jaycie <jayciegdelacruz@gmail.com>
2023-05-04 13:20:36 +0800

    Add admin orders

---
+++ app/src/main/java/com/system/gocery_final/Model/AdminOrders.java
@@ -0,0 +1,85 @@
+package com.system.gocery_final.Model;
+
+public class AdminOrders {
+
+    private String address, city, date, name, number, state, time, totalAmount;
+
+    public AdminOrders() {
+
+    }
+
+    public AdminOrders(String address, String city, String date, String name, String number, String state, String time, String totalAmount) {
+        this.address = address;
+        this.city = city;
+        this.date = date;
+        this.name = name;
+        this.number = number;
+        this.state = state;
+        this.time = time;
+        this.totalAmount = totalAmount;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(String totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
