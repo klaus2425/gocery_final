bac94ca9d5ccc51d36684e0fba4a928328a5cd91
Overseer24 <66957205+Overseer24@users.noreply.github.com>
2023-05-07 20:47:02 +0800

    Update SearchProductsActivity.java

--- app/src/main/java/com/system/gocery_final/SearchProductsActivity.java
+++ app/src/main/java/com/system/gocery_final/SearchProductsActivity.java
@@ -1,14 +1,34 @@
 package com.system.gocery_final;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SearchProductsActivity extends AppCompatActivity {
+
+    private Button SearchBtn;
+    private EditText inputText;
+    private RecyclerView searchList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_products);
+
+        inputText= (EditText) findViewById(R.id.search_product_name);
+        SearchBtn = (Button) findViewById(R.id.search_button);
+        searchList = findViewById(R.id.search_list);
+
+        searchList.setLayoutManager(new LinearLayoutManager(SearchProductsActivity.this));
+
+
+
+
+
     }
 }
