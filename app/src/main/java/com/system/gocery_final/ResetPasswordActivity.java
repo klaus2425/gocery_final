26e4249c9643a363e0ef38422b2c098046ac9b6a
Jaycie <jayciegdelacruz@gmail.com>
2023-05-15 17:24:11 +0800

    Complete forgot password functionality

--- app/src/main/java/com/system/gocery_final/ResetPasswordActivity.java
+++ app/src/main/java/com/system/gocery_final/ResetPasswordActivity.java
@@ -1,30 +1,224 @@
 package com.system.gocery_final;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.system.gocery_final.Prevalent.Prevalent;
+
+import java.util.HashMap;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
     private String check = "";
-
+    private TextView pageTitle, titleQuestions;
+    private EditText email, question1, question2;
+    private Button verifyButton;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    private AuthCredential credential;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
-
+        pageTitle = (TextView) findViewById(R.id.security_text);
+        titleQuestions = (TextView) findViewById(R.id.title_questions);
+        email = (EditText) findViewById(R.id.find_email);
+        question1 = (EditText) findViewById(R.id.question_1);
+        question2 = (EditText) findViewById(R.id.question_2);
+        verifyButton = (Button) findViewById(R.id.verify_btn);
         check = getIntent().getStringExtra("check");
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        
+        email.setVisibility(View.GONE);
+
         if(check.equals("settings")){
-            
+            titleQuestions.setText("Set Questions");
+            displayPreviousAnswers();
+
+            titleQuestions.setText("Please set Answers for the following questions");
+            verifyButton.setText("Set");
+            verifyButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setAnswers();
+                }
+            });
+
         } else if (check.equals("login")) {
-            
+            email.setVisibility(View.VISIBLE);
+
+            verifyButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    verifyUser();
+                }
+            });
+
+        }
+    }
+
+
+    private void setAnswers(){
+        String answer1 = question1.getText().toString().toLowerCase();
+        String answer2 = question2.getText().toString().toLowerCase();
+
+        if(question1.equals("")  && question2.equals("")){
+            Toast.makeText(ResetPasswordActivity.this, "Please answer the questions", Toast.LENGTH_SHORT).show();
+
+        }
+        else {
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid());
+
+            HashMap<String, Object> userdataMap = new HashMap<>();
+            userdataMap.put("answer1", answer1);
+            userdataMap.put("answer2", answer2);
+            ref.child("Security Questions").updateChildren(userdataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(ResetPasswordActivity.this, "Security questions set successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(ResetPasswordActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            });
+
+        }
+    }
+
+    private void displayPreviousAnswers(){
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid());
+
+        ref.child("Security Questions").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String ans1 = snapshot.child("answer1").getValue().toString();
+                    String ans2 = snapshot.child("answer2").getValue().toString();
+
+                    question1.setText(ans1);
+                    question2.setText(ans2);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void verifyUser(){
+        String emailUser = email.getText().toString();
+        String answer1 = question1.getText().toString().toLowerCase();
+        String answer2 = question2.getText().toString().toLowerCase();
+
+        if(!emailUser.equals("") && !answer1.equals("") && !answer2.equals("")) {
+            final DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid());
+
+            ref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        credential = EmailAuthProvider.getCredential(emailUser, snapshot.child("password").getValue().toString());
+                        String uEmail = snapshot.child("email").getValue().toString();
+                        if (snapshot.hasChild("Security Questions")) {
+                            String ans1 = snapshot.child("Security Questions").child("answer1").getValue().toString();
+                            String ans2 = snapshot.child("Security Questions").child("answer2").getValue().toString();
+
+                            if (!ans1.equals(answer1)) {
+                                Toast.makeText(ResetPasswordActivity.this, "Your first answer is incorrect", Toast.LENGTH_SHORT).show();
+                            } else if (!ans2.equals(answer2)) {
+                                Toast.makeText(ResetPasswordActivity.this, "Your second answer is incorrect", Toast.LENGTH_SHORT).show();
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(ResetPasswordActivity.this);
+                                builder.setTitle("New Password");
+
+                                final EditText newPassword = new EditText(ResetPasswordActivity.this);
+                                newPassword.setHint("Write new password here");
+                                builder.setView(newPassword);
+
+                                builder.setPositiveButton("Change", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        if (!newPassword.getText().toString().equals("")) {
+                                            user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    Log.d(TAG, "User Re-authenticated.");
+                                                    user.updatePassword(newPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            if (task.isSuccessful()) {
+                                                                Log.d(TAG, "Password changed Successfully.");
+                                                                ref.child("password").setValue(newPassword.getText().toString());
+                                                                Toast.makeText(ResetPasswordActivity.this, "Security questions set successfully", Toast.LENGTH_SHORT).show();
+                                                                Intent intent = new Intent(ResetPasswordActivity.this, LoginActivity.class);
+                                                                startActivity(intent);
+                                                            }
+                                                        }
+                                                    });
+                                                }
+                                            });
+                                        }
+                                    }
+                                });
+
+                                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                                builder.show();
+                            }
+                        } else {
+                            Toast.makeText(ResetPasswordActivity.this, "You haven't set your security questions.", Toast.LENGTH_SHORT).show();
+                        }
+                    }else {
+                        Toast.makeText(ResetPasswordActivity.this, "This email does not exist.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+        else {
+            Toast.makeText(this, "Please complete the form.", Toast.LENGTH_SHORT).show();
         }
     }
 }
