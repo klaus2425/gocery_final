69ed3870dd51b23e38b5079672ff239c2545b634
Overseer24 <66957205+Overseer24@users.noreply.github.com>
2023-05-17 04:05:56 +0800

    Update SellerRegistrationActivity.java

--- app/src/main/java/com/system/gocery_final/Sellers/SellerRegistrationActivity.java
+++ app/src/main/java/com/system/gocery_final/Sellers/SellerRegistrationActivity.java
@@ -1,31 +1,137 @@
 package com.system.gocery_final.Sellers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.system.gocery_final.Customers.LoginActivity;
 import com.system.gocery_final.Customers.MainActivity;
+import com.system.gocery_final.Customers.RegisterActivity;
 import com.system.gocery_final.R;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.system.gocery_final.R;
+
+import java.util.HashMap;
 
 public class SellerRegistrationActivity extends AppCompatActivity {
 
-    private Button sellerLoginBegin;
+    private Button sellerLoginBegin, registerButton;
+    private EditText nameInput, phoneInput, emailInput, passwordInput, addressInput;
+
+    private FirebaseAuth auth;
+    private FirebaseUser seller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_registration);
-
+        auth = FirebaseAuth.getInstance();
         sellerLoginBegin = (Button) findViewById(R.id.seller_already_have_account);
-
+        nameInput = findViewById(R.id.seller_name);
+        phoneInput = findViewById(R.id.seller_phone);
+        emailInput = findViewById(R.id.seller_email);
+        passwordInput = findViewById(R.id.seller_password);
+        addressInput = findViewById(R.id.seller_address);
         sellerLoginBegin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SellerRegistrationActivity.this, SellerLoginActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                registerSeller();
+            }
+        });
+    }
+
+    private void registerSeller() {
+        String name = nameInput.getText().toString();
+        String phone = phoneInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        String address = addressInput.getText().toString();
+
+        if(!name.equals("")&&!phone.equals("")&&!email.equals("")&&!password.equals("")&&!address.equals("")){
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+
+                        seller = auth.getCurrentUser();
+                        ValidateEmail(name,phone,password, address, email);
+                        seller.sendEmailVerification();
+                    } else {
+                        Toast.makeText(SellerRegistrationActivity.this, "Email is already in use!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        else{
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    private void ValidateEmail(String name, String contact, String password, String address, String email){
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(!(snapshot.child("Users").child(seller.getUid()).exists())){
+                    HashMap<String, Object> userdataMap = new HashMap<>();
+                    userdataMap.put("email", email);
+                    userdataMap.put("password", password);
+                    userdataMap.put("name", name);
+                    userdataMap.put("address", address);
+                    userdataMap.put("contact", contact);
+                    userdataMap.put("uid", seller.getUid());
+                    RootRef.child("Sellers").child(seller.getUid()).updateChildren(userdataMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(SellerRegistrationActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(SellerRegistrationActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Toast.makeText(SellerRegistrationActivity.this, "Network Error, Try again", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+                else {
+                    Toast.makeText(SellerRegistrationActivity.this, "This email already exists", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(SellerRegistrationActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
