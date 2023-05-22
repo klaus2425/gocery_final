2e76d7e02e67885aea2160bab6184faebd42d0da
Jaycie <jayciegdelacruz@gmail.com>
2023-05-22 13:18:40 +0800

    Update SellerNewOrdersActivity.java

--- app/src/main/java/com/system/gocery_final/Seller/SellerNewOrdersActivity.java
+++ app/src/main/java/com/system/gocery_final/Seller/SellerNewOrdersActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,6 +24,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.system.gocery_final.Model.AdminOrders;
 import com.system.gocery_final.R;
+
+import java.util.HashMap;
 
 public class SellerNewOrdersActivity extends AppCompatActivity {
 
@@ -53,7 +56,7 @@ public class SellerNewOrdersActivity extends AppCompatActivity {
                .build();
        FirebaseRecyclerAdapter<AdminOrders, AdminOrdersViewHolder> adapter = new FirebaseRecyclerAdapter<AdminOrders, AdminOrdersViewHolder>(options) {
            @Override
-           protected void onBindViewHolder(@NonNull AdminOrdersViewHolder holder, int position, @NonNull AdminOrders model) {
+           protected void onBindViewHolder(@NonNull AdminOrdersViewHolder holder, @SuppressLint("RecyclerView") int position, @NonNull AdminOrders model) {
                holder.userName.setText("Name: " + model.getName());
                holder.userPhoneNumber.setText("Phone: " + model.getNumber());
                holder.userTotalPrice.setText("Total Amount: = Php " + model.getTotalAmount());
@@ -84,10 +87,19 @@ public class SellerNewOrdersActivity extends AppCompatActivity {
                        builder.setItems(option, new DialogInterface.OnClickListener() {
                            @Override
                            public void onClick(DialogInterface dialog, int which) {
-
+                                DatabaseReference orderHistory = FirebaseDatabase.getInstance().getReference().child("Users").child(model.getUid())
+                                        .child("order history").child(model.getDate());
                                if(which == 0)
                                {
                                    String uid = getRef(position).getKey();
+                                   HashMap<String, Object> ordersMap = new HashMap<>();
+                                   ordersMap.put("date", model.getDate());
+                                   ordersMap.put("time", model.getTime());
+                                   ordersMap.put("price", model.getTotalAmount());
+                                   ordersMap.put("uid", model.getUid());
+
+                                    orderHistory.updateChildren(ordersMap);
+
                                    ordersRef.child(uid).removeValue();
                                }
                                else
