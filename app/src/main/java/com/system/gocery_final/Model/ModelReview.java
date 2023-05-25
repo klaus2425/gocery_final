f1c3c7e108a1402bbd986cf5e8304a99b5b65201
Overseer24 <66957205+Overseer24@users.noreply.github.com>
2023-05-25 23:00:33 +0800

    Update ModelReview.java

--- app/src/main/java/com/system/gocery_final/Model/ModelReview.java
+++ app/src/main/java/com/system/gocery_final/Model/ModelReview.java
@@ -1,6 +1,37 @@
 package com.system.gocery_final.Model;
 
 public class ModelReview {
+    private String uid, ratingsm, review, timestamp;
 
+    public String getUid() {
+        return uid;
+    }
 
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getRatingsm() {
+        return ratingsm;
+    }
+
+    public void setRatingsm(String ratingsm) {
+        this.ratingsm = ratingsm;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 }
