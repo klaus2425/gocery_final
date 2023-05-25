c9677222dce4d7e559d4e5b7459b4255c6360b3e
Overseer24 <66957205+Overseer24@users.noreply.github.com>
2023-05-25 14:52:58 +0800

    Update WriteReviewActivity.java

--- app/src/main/java/com/system/gocery_final/ViewHolder/WriteReviewActivity.java
+++ app/src/main/java/com/system/gocery_final/ViewHolder/WriteReviewActivity.java
@@ -3,18 +3,45 @@ package com.system.gocery_final.ViewHolder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.system.gocery_final.R;
 
 public class WriteReviewActivity extends AppCompatActivity {
 
     private String pid;
+
+    private ImageView prodImage;
+    private ImageButton backBtn;
+    private TextView prodName;
+    private RatingBar ratingBar;
+    private EditText review;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review);
 
+        backBtn = findViewById(R.id.backBtn);
+        prodImage = findViewById(R.id.product_image_details);
+        prodName = findViewById(R.id.product_name_details);
+        ratingBar = findViewById(R.id.ratingBar);
+        review = findViewById(R.id.reviewEt);
+
+
         pid = getIntent().getStringExtra("pid");
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
     }
 }
