4a1d688173264986d5156ca6c0cc5d6e1df80614
Jaycie <jayciegdelacruz@gmail.com>
2023-05-29 10:34:31 +0800

    Add Seller History Activity

---
+++ app/src/main/java/com/system/gocery_final/Seller/SellerHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.system.gocery_final.Seller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.system.gocery_final.R;
+
+public class SellerHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_history);
+    }
+}
